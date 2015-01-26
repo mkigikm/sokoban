5347d3dbab2ed7debb5e4e8163bd9172b14d218a
Matthew Hicks <mkigikm@gmail.com>
2015-01-25 19:13:44 -0500

    improves SokobanLevel#to_s to show player and box positions

--- sokoban_level.rb
+++ sokoban_level.rb
@@ -11,20 +11,28 @@ class SokobanLevel
   PLAYERS = [PLAYER, PLAYER_ON_GOAL]
   BOXES = [BOX, BOX_ON_GOAL]
 
+  def self.from_file(filename)
+    SokobanLevel.new.read_file(filename)
+  end
+
   def read_file(filename)
     lines = File.readlines(filename).map(&:chomp)
 
     @boxes = []
     @grid = lines.each_with_index.map { |line, row| read_line(line, row) }
+
+    self
   end
 
   def to_s
-    @grid.collect do |row|
-      row.collect do |square|
+    @grid.each_with_index.collect do |row_squares, row|
+      row_squares.each_with_index.collect do |square, col|
+        pos = [row, col]
+
         case square
         when :wall then WALL
-        when :goal then GOAL
-        when :floor then FLOOR
+        when :goal then display_goal(square, pos)
+        when :floor then display_floor(square, pos)
         end
       end.join
     end.join("\n")
@@ -45,6 +53,26 @@ class SokobanLevel
       else
         :floor
       end
+    end
+  end
+
+  def display_goal(square, pos)
+    if @player_pos == pos
+      PLAYER_ON_GOAL
+    elsif @boxes.include?(pos)
+      BOX_ON_GOAL
+    else
+      GOAL
+    end
+  end
+
+  def display_floor(square, pos)
+    if @player_pos == pos
+      PLAYER
+    elsif @boxes.include?(pos)
+      BOX
+    else
+      FLOOR
     end
   end
 end
